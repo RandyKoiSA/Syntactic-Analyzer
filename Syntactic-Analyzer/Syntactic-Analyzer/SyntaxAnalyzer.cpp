f14ebf7a854ccb63f33679ea8aca1b0d823ef976
Randy Le <97randy.le@gmail.com>
2019-04-16 13:28:42 -0700

    update 1.2

--- Syntactic-Analyzer/Syntactic-Analyzer/SyntaxAnalyzer.cpp
+++ Syntactic-Analyzer/Syntactic-Analyzer/SyntaxAnalyzer.cpp
@@ -4,33 +4,156 @@
 #include <iostream>
 #include "SyntaxAnalyzer.h"
 
+using namespace std;
+
 SyntaxAnalyzer::SyntaxAnalyzer() {
-	syntaxStack = std::stack<std::string>();
-	syntaxStack.push("$");
-	lexemes = std::vector<std::string>();
-	tokens = std::vector<std::string>();
+	index = 0;
+	syntaxStack = stack<string>();
+	lexemes = vector<string>();
+	tokens = vector<string>();
+	makeConversions();
 }
 
-void SyntaxAnalyzer::loadLexemes(std::vector<std::string> lexemes) {
+void SyntaxAnalyzer::makeConversions() {
+	conversion["E"] = 0;
+	conversion["J"] = 1;
+	conversion["T"] = 2;
+	conversion["U"] = 3;
+	conversion["F"] = 4;
+	conversion["i"] = 0;
+	conversion["+"] = 1;
+	conversion["-"] = 2;
+	conversion["/"] = 3;
+	conversion["*"] = 4;
+	conversion["("] = 5;
+	conversion[")"] = 6;
+	conversion["$"] = 7;
+}
+
+void SyntaxAnalyzer::loadLexemes(vector<string> lexemes) {
 	this->lexemes = lexemes;
 }
 
-void SyntaxAnalyzer::loadTokens(std::vector < std::string> tokens) {
+void SyntaxAnalyzer::loadTokens(vector<string> tokens) {
 	this->tokens = tokens;
 }
 
 void SyntaxAnalyzer::printLexemes() {
 	for (int i = 0; i < lexemes.size(); i++) {
-		std::cout << lexemes[i] << std::endl;
+		cout << lexemes[i] << endl;
 	}
 }
 
 void SyntaxAnalyzer::printTokens() {
 	for (int i = 0; i < tokens.size(); i++) {
-		std::cout << tokens[i] << std::endl;
+		cout << tokens[i] << endl;
 	}
 }
 
 void SyntaxAnalyzer::lexer() {
+	index++;
+}
 
+void SyntaxAnalyzer::analyzeSyntax() {
+	if (lexemes.size() < 0 || tokens.size() < 0) {
+		cout << "ERROR: NO FILE WAS FOUND TO ANALYZE SYNTAX\n";
+		return;
+	}
+	// add $ to the back
+	lexemes.push_back("$");
+	tokens.push_back("$");
+	syntaxStack.push("$");
+
+	// adding first state in stack
+	syntaxStack.push("E");
+
+	do {
+		int currentRow = conversion[syntaxStack.top()];
+		int currentColumn = conversion[lexemes[index]];
+		syntaxStack.pop();
+		int state = rules[currentRow][currentColumn];
+		
+		switch (state){
+		case -1:
+			break;
+		case 0:
+			break;
+		case 1:
+			break;
+		case 2:
+			break;
+		case 3:
+			break;
+		case 4:
+			break;
+		case 5:
+			break;
+		case 6:
+			break;
+		case 7:
+			break;
+		case 8:
+			break;
+		default:
+			cout << "ERROR, UNREACHABLE AREA\n";
+		}
+
+	} while (syntaxStack.top() == "$" || lexemes[index] == "$");
+}
+
+void SyntaxAnalyzer::ruleOne()
+{
+	// pushes J then T
+}
+
+void SyntaxAnalyzer::ruleTwo()
+{
+	// push J, T, +
+	// check if the top of stack terminal and compares with lexeme index
+}
+
+void SyntaxAnalyzer::ruleThree()
+{
+	// push J, T, -
+	// check if the top of stack terminal and compares with lexeme index
+}
+
+void SyntaxAnalyzer::ruleFour()
+{
+	// push U, F
+}
+
+void SyntaxAnalyzer::ruleFive()
+{
+	// push U, F, *
+	// check if the top of stack terminal and compares with lexeme index
+}
+
+void SyntaxAnalyzer::ruleSix()
+{
+	// push U, F, /
+	// check if the top of stack terminal and compares with lexeme index
+}
+
+void SyntaxAnalyzer::ruleSeven()
+{
+	// push ), E, (
+	// check if the top of stack terminal and compares with lexeme index
+
+}
+
+void SyntaxAnalyzer::ruleEight()
+{
+	// push i
+	// check if the top of stack terminal and compares with lexeme index
+}
+
+void SyntaxAnalyzer::ruleZero()
+{
+	// push nothing; continue
+}
+
+void SyntaxAnalyzer::ruleError()
+{
+	// ERROR HAS OCCURED
 }
--- Syntactic-Analyzer/Syntactic-Analyzer/SyntaxAnalyzer.h
+++ Syntactic-Analyzer/Syntactic-Analyzer/SyntaxAnalyzer.h
@@ -2,19 +2,48 @@
 #include <stack>
 #include <vector>
 #include <string>
+#include <map>
 
 using namespace std;
+
 class SyntaxAnalyzer {
 public:
 	SyntaxAnalyzer();
+	// main method of running syntax analyzer
+	void analyzeSyntax();
+	void ruleOne();   // TJ
+	void ruleTwo();   // +TJ
+	void ruleThree(); // -TJ
+	void ruleFour();  // FU
+	void ruleFive();  // *FU
+	void ruleSix();   // /FU
+	void ruleSeven(); // (E)
+	void ruleEight(); // i
+	void ruleZero();  // EPSILON
+	void ruleError(); // ERROR
+
+	// load lexemes / tokens for the lexeranalyzer
 	void loadLexemes(vector<string>);
 	void loadTokens(vector<string>);
+	// get next lexeme / token
+	void lexer();
+	// testable methods
 	void printLexemes();
 	void printTokens();
-	void lexer();
+
 private:
 	vector<string> lexemes;
 	vector<string> tokens;
 	stack <string> syntaxStack; 
-	
+	map<string, int> conversion;
+	int rules[5][8] = {
+		 1, -1, -1, -1, -1,  1, 0, 0,
+		-1,  2,  3, -1, -1, -1, 0, 0,
+		 4,  0,  0, -1, -1,  4, 0, 0,
+		-1,  0,  0,  6,  5, -1, 0, 0,
+		 8,  0,  0,  0,  0,  7, 0, 0
+	};
+	int index;	// index will keep track of where you are in the lexeme/token
+
+	void makeConversions();
 };
